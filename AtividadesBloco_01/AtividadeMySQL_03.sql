0e1f97ce36d7fa6d122e5d0f4a81be90def99b39
PipitoDev <joaovitorsguizzato@gmail.com>
2026-06-13 18:42:11 -0300

    Fix price unit format in SQL query

--- AtividadesBloco_01/AtividadeMySQL_03.sql
+++ AtividadesBloco_01/AtividadeMySQL_03.sql
@@ -1,6 +1,6 @@
 SELECT OrderDetailID AS 'Detalhes da Encomenda',
 	   Quantity AS 'Quantidade',
-       CONCAT('R$9.90') AS 'Preço Unitário',
+       'R$9.90' AS 'Preço Unitário',
        CONCAT('R$', CONVERT(Quantity, DECIMAL(10, 0)) * 9.90) AS 'Preço Total'
 FROM OrderDetails
 LIMIT 15;
